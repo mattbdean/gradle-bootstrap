ad1399a421fac2cae4ed82079a57df7d5d279a2e
Matthew Dean <deanmatthew16@gmail.com>
2014-12-25 16:25:11 -0500

    Dynamically name the projects

--- api/src/test/kotlin/net/dean/gbs/CreationTest.kt
+++ api/src/test/kotlin/net/dean/gbs/CreationTest.kt
@@ -8,25 +8,57 @@ import java.nio.file.SimpleFileVisitor
 import java.nio.file.attribute.BasicFileAttributes
 import java.nio.file.FileVisitResult
 import java.io.IOException
-import java.nio.charset.StandardCharsets
 import org.testng.Assert
 
 public class CreationTest {
     public test fun basicCreate() {
-        val projName = "Example"
-        val projPath = Paths.get("/home/matthew/test/abc/$projName")
-        delete(projPath)
-        val proj = Project(projName, "com.example.app")
+        val (proj, path) = newProject()
         proj.license = License.APACHE
         proj.add(Language.JAVA)
         proj.build.logging = LoggingFramework.SLF4J
         proj.build.testing = TestingFramework.TESTNG
         proj.build.projectContext.add(Repository.MAVEN_CENTRAL)
+        // Random plugin
         proj.build.addGradlePlugin(Dependency("net.swisstech", "gradle-dropwizard", scope = Scope.CLASSPATH))
         proj.build.plugins.add("application")
 
-        Exporter().export(proj, projPath, ProjectRenderer().render(proj))
-        validateGradleBuild(projPath)
+        // Render and export the project
+        Exporter().export(proj, path, ProjectRenderer().render(proj))
+        validateGradleBuild(path)
+    }
+
+    /**
+     * Executes "gradle build" in a given directory and asserts that the exit code of that process is equal to 0.
+     */
+    private fun validateGradleBuild(rootPath: Path) {
+        val command = array("gradle", "build")
+        val dir = rootPath.toFile()
+        println("Executing command '${command.join(" ")}' in directory '${dir.getAbsolutePath()}'")
+        val process = ProcessBuilder()
+                .directory(dir)
+                .command(*command)
+                .inheritIO()
+                .start()
+        // Should have an exit value of 0
+        val exitCode = process.waitFor()
+        println("Finished")
+        Assert.assertEquals(exitCode, 0)
+    }
+
+    /**
+     * Returns a Pair of a Project to its root Pat based on the calling method. The group will be
+     * "com.example.$callingMethodName" and its base path will be "build/projects/$callingMethodName".
+     *
+     * stackOffset is the amount of indicies to increase the base count by. A stackOffset of 0 would result in the
+     * method that directly called this method, 1 would be the method that called the other method, etc.
+     */
+    private fun newProject(stackOffset: Int = 0): Pair<Project, Path> {
+        val name = Thread.currentThread().getStackTrace()[2 + stackOffset].getMethodName()
+        val path = Paths.get("build/projects/$name")
+        val proj = Project(name, "com.example.$name")
+        // Delete the files before generating it so that if we want to examine the crated files after creation, we can.
+        delete(path)
+        return proj to path
     }
 
     private fun delete(path: Path) {
@@ -49,23 +81,5 @@ public class CreationTest {
             // Delete file
             Files.delete(path)
         }
-    }
-
-    /**
-     * Executes "gradle build" in a given directory and asserts that the exit code of that process is equal to 0.
-     */
-    public fun validateGradleBuild(rootPath: Path) {
-        val command = array("gradle", "build")
-        val dir = rootPath.toFile()
-        println("Executing command '${command.join(" ")}' in directory '${dir.getAbsolutePath()}'")
-        val process = ProcessBuilder()
-                .directory(dir)
-                .command(*command)
-                .inheritIO()
-                .start()
-        // Should have an exit value of 0
-        val exitCode = process.waitFor()
-        println("Finished")
-        Assert.assertEquals(exitCode, 0)
     }
 }
