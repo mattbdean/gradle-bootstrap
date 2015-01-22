5dd87f5af1550e418dbfd2224e7e2eb0aee6c14f
Matthew Dean <deanmatthew16@gmail.com>
2015-01-22 09:16:22 -0500

    Add sitemap and robots.txt

--- service/src/main/kotlin/net/dean/gbs/web/resources/IndexResource.kt
+++ service/src/main/kotlin/net/dean/gbs/web/resources/IndexResource.kt
@@ -7,11 +7,39 @@ import net.dean.gbs.web.views.IndexView
 import javax.ws.rs.GET
 
 Path("/")
-Produces(MediaType.TEXT_HTML)
 public class IndexResource {
+    Produces(MediaType.TEXT_HTML)
     GET public fun get(): IndexView = IndexView()
 
     Path("googlec536b80d9a503e51.html")
+    Produces(MediaType.TEXT_HTML)
     GET public fun googleWebmaster(): String =
         """google-site-verification: googlec536b80d9a503e51.html"""
+
+    Path("sitemap.xml")
+    Produces(MediaType.TEXT_XML)
+    GET public fun sitemap(): String =
+"""<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<url>
+  <loc>http://gradle-bootstrap.herokuapp.com/</loc>
+  <lastmod>2015-01-22T14:00:39+00:00</lastmod>
+  <changefreq>weekly</changefreq>
+</url>
+</urlset>
+"""
+
+    Produces(MediaType.TEXT_PLAIN)
+    Path("robots.txt")
+    GET public fun robotsTxt(): String =
+"""User-Agent: *
+Disallow: /project/*/download
+Disallow: /admin
+
+Sitemap: http://gradle-bootstrap.herokuapp.com/sitemap.xml
+"""
 }
